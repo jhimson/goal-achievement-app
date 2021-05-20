dc4f8e0f9b71b6166df811f2a0b7cb4b234ce831
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-05-19 20:09:57 -0400

    fix(model) replace email field into username field for user queries

--- server/db/models/userModel.js
+++ server/db/models/userModel.js
@@ -4,40 +4,26 @@ const db = require("..");
 
 const salt = bcrypt.genSaltSync(10);
 
-const fetchUsers = () => db.query("SELECT * FROM tbl_users");
-
-const findEmail = (email) =>
+const findUser = (username) =>
   db.query({
-    text: `SELECT * FROM tbl_users WHERE email = $1`,
-    values: [email],
+    text: `SELECT * FROM tbl_users WHERE username = $1`,
+    values: [username],
   });
 
+const fetchUsers = () => db.query("SELECT * FROM tbl_users");
+
 const createNewUser = async (user) => {
-  const { user_id, firstname, lastname, email, password } = user;
+  const { user_id, firstname, lastname, username, password } = user;
   const hashedPassword = bcrypt.hashSync(password, salt);
 
   return db.query({
-    text: `INSERT INTO tbl_users (user_id, firstname, lastname, email, password, is_admin, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, DEFAULT, DEFAULT, DEFAULT) returning *`,
-    values: [user_id, firstname, lastname, email, hashedPassword],
+    text: `INSERT INTO tbl_users (user_id, firstname, lastname, username, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, DEFAULT, DEFAULT, DEFAULT) returning *`,
+    values: [user_id, firstname, lastname, username, hashedPassword],
   });
 };
 
-const createNewMember = async (user_id, member_id) =>
-  db.query({
-    text: `INSERT INTO tbl_members (member_id, user_id, role) VALUES ($1, $2, DEFAULT)`,
-    values: [member_id, user_id],
-  });
-
-const findUser = (email) =>
-  db.query({
-    text: `SELECT * FROM tbl_users WHERE email = $1`,
-    values: [email],
-  });
-
 module.exports = {
   fetchUsers,
-  findEmail,
   createNewUser,
   findUser,
-  createNewMember,
 };
