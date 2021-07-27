be647da702ba987fc36a019d3f315a2f29577f9e
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-07-27 14:27:02 -0400

    feat: implemement re-usable modal component to long term goals list

--- client/src/pages/LongTermGoalsPage.jsx
+++ client/src/pages/LongTermGoalsPage.jsx
@@ -13,8 +13,8 @@ import { IoTrash } from 'react-icons/io5';
 
 //! COMPONENTS
 import { useToasts } from 'react-toast-notifications';
-import ConfirmDeleteLongTermGoalModal from '../components/ConfirmDeleteLongTermGoalModal';
 import Layout from '../components/Layout';
+import Modal from '../components/Modal';
 import Spinner from '../components/Spinner';
 //! ----------------------------------------------------->
 
@@ -96,11 +96,14 @@ const LongTermGoalsPage = () => {
   return (
     <Layout active="longTermGoalsPage">
       <div className="relative flex flex-col items-center min-h-full bg-gray-100">
-        <ConfirmDeleteLongTermGoalModal
-          isModalVisible={isModalVisible}
-          setIsModalVisible={setIsModalVisible}
-          deleteGoal={() => dispatch(deleteLongTermGoal(idToDelete))}
-        />
+        {isModalVisible ? (
+          <Modal
+            title="long term goal"
+            isVisible={isModalVisible}
+            setIsVisible={setIsModalVisible}
+            deleteFunc={() => dispatch(deleteLongTermGoal(idToDelete))}
+          />
+        ) : null}
         <div className="w-full p-3 bg-gray-100 xl:p-10 lg:w-11/12 ">
           <div className="h-auto p-5 bg-green-400 rounded shadow">
             <h2 className="mb-10 font-mono text-2xl font-extrabold text-center text-gray-800 md:text-4xl">
